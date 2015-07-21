5188d7c453cb26b82ac014bf2d172ff0717bb92b
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2015-07-21 16:16:02 +0300

    rebuild on push

--- push.php
+++ push.php
@@ -1,5 +1,6 @@
 <?php
 
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
+if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
+{
   shell_exec( 'cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && jekyll build && git reset --hard HEAD && git pull origin master' );
 }
