5c40cca8a34bdf93d1b1069a2c73f84bb0ffb983
Yuriy Myrosh <Konro1@mail.ru>
2015-07-21 18:21:53 +0300

    Update push.php

--- push.php
+++ push.php
@@ -2,7 +2,7 @@
 
 //if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
 //{
-  system( 'cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && git reset --hard HEAD && git pull origin master' );
+  system( 'cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && git pull origin master' );
   echo json_encode(array('success' => true));
   die;
 //}
