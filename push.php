ea842250b14826eba0daa6ab42c190f8d1277078
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2015-07-21 16:47:12 +0300

    pushes hook

--- push.php
+++ push.php
@@ -4,3 +4,4 @@ if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
 {
   shell_exec( 'cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && jekyll build && git reset --hard HEAD && git pull origin master' );
 }
+
