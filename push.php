ce1a855a9177dbd0e67617f1f9104cda73b83b06
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2015-07-22 12:06:14 +0300

    github hook

--- push.php
+++ push.php
@@ -2,8 +2,9 @@
 
 if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
 {
-  shell_exec( 'cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && jekyll build && git reset --hard HEAD && git pull origin master' );
-  echo json_encode(array('success' => true));
+  exec('cd /var/www/vhosts/lemberg.co.uk/howto.lemberg.co.uk && git reset --hard HEAD');
+  exec('git pull  2>&1 origin master', $output, $code);
+  echo json_encode(array('success' => true, 'git_output' => $output, 'git_code' => $code));
   die;
 }
 
